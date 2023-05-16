9dbb64a0ab054016d7510e37b17ae4dd21ad4dc2
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-05-16 15:07:47 +1000

    Updates

--- src/App.js
+++ src/App.js
@@ -3,39 +3,44 @@ import Searchbar from './components/Searchbar';
 import Nav from './components/Nav';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import React from 'react';
+import Home from './routes/Home';
 const router = createBrowserRouter([
     {
       path: "/home",
-      element: <p>Home</p>
+      element: <Home/>,
+      searchRestriction: null
     },
     {
       path: "/movie",
-      element: <p>Movies</p>
+      element: <p>Movies</p>,
+      searchRestriction: "Movies"
     },
     {
       path: "/tv",
-      element: <p>TV</p>
+      element: <p>TV</p>,
+      searchRestriction: "TV Series"
     },
     {
       path: "/bookmark",
-      element: <p>Bookmark</p>
+      element: <p>Bookmark</p>,
+      searchRestriction: "Bookmarks"
     },
   ])
 
 export default function App(){
     const [searchValue, updateSearchValue] = React.useState("");
     
-    updateSearchValue(()=>{
-        console.log("e")
-    })
+    function handleChange(e){
+      updateSearchValue(e.target.value)
+    }
 
     return(<>
         <div id="LHS">
             <Nav/>
         </div>
         <div id="RHS">
-            <Searchbar value={searchValue} onchange={()=>{updateSearchValue()}}/>
-            <RouterProvider router={router}/>
+            <Searchbar value={searchValue} onchange={(e)=>{handleChange(e)}}/>
+            {searchValue===""&&<RouterProvider router={router}/>}
         </div>
     </>
     )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@
 export default function Home(){
     return(
         <main>
-            
+            <h1>Trending</h1>
         </main>
     )
 }
