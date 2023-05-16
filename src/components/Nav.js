19337823e1dd9dcd6eef611dfc939b78540fe469
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-05-16 12:54:12 +1000

    Nav Improvements

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,12 +10,14 @@ export default function Nav(){
         <nav>
             <img className="nav--logo" src={navLogo}></img>
             <div className="nav--secondaryIcons">
-                <a href="/home"><img className="nav--logo" style={window.location.pathname==="/home"?{filter: "brightness(10)"}:{}} src={navHome}></img></a>
-                <a href="/movie"><img className="nav--logo" style={window.location.pathname==="/movie"?{filter: "brightness(10)"}:{}} src={navMovie}></img></a>
-                <a href="/tv"><img className="nav--logo" style={window.location.pathname==="/tv"?{filter: "brightness(10)"}:{}} src={navTv}></img></a>
-                <a href="/bookmark"><img className="nav--logo" style={window.location.pathname==="/bookmark"?{filter: "brightness(10)"}:{}} src={navBookmark}></img></a>
+                <a href="/home"><img className={`nav--icon ${window.location.pathname==="/home"?"nav--iconHighlighted":""}`} src={navHome}></img></a>
+                <a href="/movie"><img className={`nav--icon ${window.location.pathname==="/movie"?"nav--iconHighlighted":""}`} src={navMovie}></img></a>
+                <a href="/tv"><img className={`nav--icon ${window.location.pathname==="/tv"?"nav--iconHighlighted":""}`} src={navTv}></img></a>
+                <a href="/bookmark"><img className={`nav--icon ${window.location.pathname==="/bookmark"?"nav--iconHighlighted":""}`} src={navBookmark}></img></a>
             </div>
             <img src={navAvatar} id="nav--avatar"></img>
         </nav>
     )
 }
+
+//More accurate way to make SVG files change color https://stackoverflow.com/questions/22252472/how-can-i-change-the-color-of-an-svg-element
